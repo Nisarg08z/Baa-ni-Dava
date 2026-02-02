a36adb37091c1d021330b250f578faf3b02fcac4
Nisarg08z <nisarg693z@gmail.com>
2026-02-02 23:52:41 +0530

    Day - 2.2

--- Client/src/data/medicines.js
+++ Client/src/data/medicines.js
@@ -1,16 +1,36 @@
 export const stores = [
-    { _id: "1", name: "Apollo Pharmacy" },
-    { _id: "2", name: "MedPlus" },
-    { _id: "3", name: "Wellness Forever" }
+  { _id: "1", name: "Ayurvedic" },
+  { _id: "2", name: "Homeopathy" }
 ];
 
+
 export const medicines = [
-    { _id: "101", name: "Paracetamol 500mg", store: stores[0] },
-    { _id: "102", name: "Amoxicillin 500mg", store: stores[0] },
-    { _id: "103", name: "Cetirizine 10mg", store: stores[1] },
-    { _id: "104", name: "Ibuprofen 400mg", store: stores[1] },
-    { _id: "105", name: "Metformin 500mg", store: stores[2] },
-    { _id: "106", name: "Atorvastatin 10mg", store: stores[2] },
-    { _id: "107", name: "Pantoprazole 40mg", store: stores[0] },
-    { _id: "108", name: "Aspirin 75mg", store: stores[1] }
+  // Ayurvedic
+  { _id: "101", name: "G Ark", store: stores[0] },
+  { _id: "102", name: "Pit Har", store: stores[0] },
+  { _id: "103", name: "PLT", store: stores[0] },
+  { _id: "104", name: "BVM", store: stores[0] },
+  { _id: "105", name: "TCI2", store: stores[0] },
+  { _id: "106", name: "AV", store: stores[0] },
+  { _id: "107", name: "T2", store: stores[0] },
+  { _id: "108", name: "P2", store: stores[0] },
+  { _id: "109", name: "Arbuda B", store: stores[0] },
+  { _id: "110", name: "LP Course", store: stores[0] },
+  { _id: "111", name: "Gravila", store: stores[0] },
+  { _id: "112", name: "Divyaprash", store: stores[0] },
+  { _id: "113", name: "Aimzyme", store: stores[0] },
+  { _id: "114", name: "Manjishthadi Qath", store: stores[0] },
+  { _id: "115", name: "Green Fero", store: stores[0] },
+  { _id: "116", name: "PPK", store: stores[0] },
+  { _id: "117", name: "NZR", store: stores[0] },
+  { _id: "118", name: "Savsth Churn", store: stores[0] },
+  { _id: "119", name: "Point", store: stores[0] },
+  { _id: "120", name: "Amruta", store: stores[0] },
+
+  // Homeopathy
+  { _id: "121", name: "Imfla Z", store: stores[1] },
+  { _id: "122", name: "Tomour Z", store: stores[1] },
+  { _id: "123", name: "Blood Drop", store: stores[1] },
+  { _id: "124", name: "BC 28", store: stores[1] }
 ];
+
